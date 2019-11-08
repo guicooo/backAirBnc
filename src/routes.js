ade6b8b341415dcfa2798aeb7893a9cad8c41d31
Guilherme <Cordeiro>
2019-11-08 17:04:08 -0300

    correcoes upload

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const multer = requie('multer');
+const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
