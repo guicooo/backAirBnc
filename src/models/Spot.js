df1e57c7fe2177b867f5d8025dc228ad937663e3
Guilherme <guico_@hotmail.com>
2019-11-12 17:38:56 -0300

    add url file

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -9,6 +9,17 @@ const SpotSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }
-});
+},  
+{
+    toJSON: {
+        virtuals: true
+    }
+}
+
+);
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `https://omminiback.herokuapp.com/files/${this.thumbnail}`
+})
 
  module.exports = mongoose.model('Spot', SpotSchema);
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const cool = require('cool-ascii-faces')
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors");
+const path = require('path');
 const routes = require('./routes');
 const PORT = process.env.PORT || 3333
 
@@ -23,6 +24,7 @@ app
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '...', 'uploads')))
 app.use(routes);
 
 
